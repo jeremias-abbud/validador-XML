d1056207b856d9d72723668f737edc1072e7ad0f
jeremias-abbud <jeremias.abbud@gmail.com>
2024-05-20 10:45:01 -0300

    Layout aprimorado, funções aprimoradas

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let globalBeneficiaryNames = []; // Variável global para armazenar os nomes dos beneficiários
 
+document.getElementById('xmlFileInput').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const xmlDoc = parseXML(e.target.result);
+            const feedback = document.getElementById('feedback');
+            if (isValidXML(xmlDoc)) {
+                feedback.textContent = "Arquivo XML carregado com sucesso.";
+                feedback.classList.add('success');
+                const beneficiaryNames = getBeneficiaryNames(xmlDoc);
+                displayBeneficiaryInfo(beneficiaryNames);
+            } else {
+                feedback.textContent = "Erro: Arquivo XML inválido.";
+                feedback.classList.remove('success');
+                feedback.classList.add('error');
+            }
+        };
+        reader.readAsText(file);
+    }
+});
 
 function parseXML(xml) {
     const parser = new DOMParser();
@@ -35,30 +56,8 @@ function getBeneficiaryNames(xmlDoc) {
         beneficiaryNames.push(nomeBeneficiario);
     }
 
-
     return beneficiaryNames;
 }
-
-document.getElementById('xmlFileInput').addEventListener('change', function (event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            const xmlDoc = parseXML(e.target.result);
-            const feedback = document.getElementById('feedback');
-            if (isValidXML(xmlDoc)) {
-                feedback.textContent = "Arquivo XML carregado com sucesso.";
-                feedback.classList.add('success');
-                const beneficiaryNames = getBeneficiaryNames(xmlDoc);
-                displayBeneficiaryInfo(beneficiaryNames);
-            } else {
-                feedback.textContent = "Erro: Arquivo XML inválido.";
-                feedback.classList.remove('success');
-            }
-        };
-        reader.readAsText(file);
-    }
-});
 
 function displayBeneficiaryInfo(beneficiaryNames) {
     const output = document.getElementById('output');
@@ -66,25 +65,21 @@ function displayBeneficiaryInfo(beneficiaryNames) {
     globalBeneficiaryNames = beneficiaryNames; // Atribui os nomes dos beneficiários à variável global
     beneficiaryNames.forEach((name, index) => {
         output.innerHTML += `<p>Conta ${index + 1}: ${name}</p>`;
-        // Remover a classe 'hidden' do botão Limpar Resultado
-    document.getElementById('clearResultBtn').classList.remove('hidden');
     });
+    // Remover a classe 'hidden' do botão Limpar Resultado
+    document.getElementById('clearResultBtn').classList.remove('hidden');
 }
-
 
 function exibirBeneficiarios() {
     displayBeneficiaryInfo(globalBeneficiaryNames); // Usando a variável global que armazena os nomes dos beneficiários
 }
-
 
 function validateTag() {
     clearFeedback();
     const tagName = document.getElementById('tagNameInput').value.trim();
     const feedback = document.getElementById('feedback');
     const validationResult = document.getElementById('validationResult');
-    const historyContent = document.getElementById('historyContent');
     const clearResultBtn = document.getElementById('clearResultBtn');
-    const historyBtn = document.getElementById('historyBtn');
 
     if (tagName === "") {
         feedback.textContent = "Por favor, digite o nome da TAG.";
@@ -99,28 +94,9 @@ function validateTag() {
             const xmlDoc = parseXML(e.target.result);
             if (isValidXML(xmlDoc)) {
                 const beneficiaryNames = getBeneficiaryNames(xmlDoc);
-                const absentAccounts = [];
-                beneficiaryNames.forEach((name, index) => {
-                    const conta = xmlDoc.evaluate(`//*[local-name()='guiaSP-SADT' or local-name()='guiaResumoInternacao' or local-name()='guiaConsulta'][${index + 1}]//*[local-name()='${tagName}']`, xmlDoc, null, XPathResult.ANY_TYPE, null);
-                    if (!conta.iterateNext()) {
-                        absentAccounts.push(name);
-                    }
-                });
-
-                if (absentAccounts.length > 0) {
-                    validationResult.innerHTML = `<p>Tag <span class="bold">${tagName}</span> ausente nas seguintes contas:</p>`;
-                    absentAccounts.forEach(name => {
-                        validationResult.innerHTML += `<p>${name}</p>`;
-                    });
-                } else {
-                    validationResult.innerHTML = `<p>A tag <span class="bold">${tagName}</span> está presente em todas as contas.</p>`;
-                }
-
-                feedback.textContent = "";
+                const result = validateTagPresence(xmlDoc, beneficiaryNames, tagName);
+                displayValidationResult(result);
                 clearResultBtn.classList.remove('hidden');
-                historyBtn.classList.remove('hidden'); // Remover classe hidden do botão de histórico
-                historyContent.innerHTML += `<div><strong>Validação:</strong> ${tagName} - ${new Date().toLocaleString()}</div>`;
-                historyContent.innerHTML += validationResult.innerHTML;
             } else {
                 feedback.textContent = "Erro: Arquivo XML inválido.";
             }
@@ -131,44 +107,56 @@ function validateTag() {
     }
 }
 
+function validateTagPresence(xmlDoc, beneficiaryNames, tagName) {
+    const namespaces = {
+        ans: "http://www.ans.gov.br/padroes/tiss/schemas"
+    };
 
-function toggleHistory() {
-    const history = document.getElementById('history');
-    history.classList.toggle('hidden');
+    const guiaSP_SADT = xmlDoc.getElementsByTagNameNS(namespaces.ans, "guiaSP-SADT");
+    const guiaResumoInternacao = xmlDoc.getElementsByTagNameNS(namespaces.ans, "guiaResumoInternacao");
+    const guiaConsulta = xmlDoc.getElementsByTagNameNS(namespaces.ans, "guiaConsulta");
+
+    const results = [];
+
+    for (let i = 0; i < guiaSP_SADT.length; i++) {
+        const tags = guiaSP_SADT[i].getElementsByTagNameNS(namespaces.ans, tagName);
+        const isPresent = tags.length > 0;
+        results.push({ name: beneficiaryNames[i], isPresent });
+    }
+
+    for (let i = 0; i < guiaResumoInternacao.length; i++) {
+        const tags = guiaResumoInternacao[i].getElementsByTagNameNS(namespaces.ans, tagName);
+        const isPresent = tags.length > 0;
+        results.push({ name: beneficiaryNames[i + guiaSP_SADT.length], isPresent });
+    }
+
+    for (let i = 0; i < guiaConsulta.length; i++) {
+        const tags = guiaConsulta[i].getElementsByTagNameNS(namespaces.ans, tagName);
+        const isPresent = tags.length > 0;
+        results.push({ name: beneficiaryNames[i + guiaSP_SADT.length + guiaResumoInternacao.length], isPresent });
+    }
+
+    return results;
 }
 
-function clearFile() {
-    clearFeedback();
-    document.getElementById('xmlFileInput').value = "";
-    document.getElementById('output').innerHTML = "";
-    document.getElementById('validationResult').innerHTML = "";
-    document.getElementById('feedback').innerHTML = "";
-    document.getElementById('clearResultBtn').classList.add('hidden');
-    document.getElementById('historyBtn').classList.add('hidden');
-}
 
-function clearResult() {
-    clearFeedback();
-    document.getElementById('output').innerHTML = "";
-    document.getElementById('validationResult').innerHTML = "";
-    document.getElementById('feedback').innerHTML = "";
-    document.getElementById('clearResultBtn').classList.add('hidden');
-    document.getElementById('historyBtn').classList.add('hidden');
-}
-
-function clearTag() {
-    document.getElementById('tagNameInput').value = "";
+function displayValidationResult(results) {
+    const tagName = document.getElementById('tagNameInput').value.trim();
+    const validationResult = document.getElementById('validationResult');
+    validationResult.innerHTML = `<h2>Resultado da Validação por TAG: ${tagName}</h2>`;
+    results.forEach(result => {
+        const status = result.isPresent ? "<span class='green-text'>Presente</span>" : "<span class='red-text'>Ausente</span>";
+        validationResult.innerHTML += `<p>Beneficiário: ${result.name} - TAG ${status}</p>`;
+    });
 }
 
 function compararContas() {
     clearFeedback();
+    const file = document.getElementById('xmlFileInput').files[0];
     const feedback = document.getElementById('feedback');
     const validationResult = document.getElementById('validationResult');
-    const historyContent = document.getElementById('historyContent');
     const clearResultBtn = document.getElementById('clearResultBtn');
-    const historyBtn = document.getElementById('historyBtn');
 
-    const file = document.getElementById('xmlFileInput').files[0];
     if (file) {
         feedback.textContent = "Comparando contas...";
         const reader = new FileReader();
@@ -197,7 +185,7 @@ function compararContas() {
                 });
 
                 // Listar todas as tags e verificar a presença em cada conta
-                validationResult.innerHTML = "<p>Listagem de todas as TAGS das contas:</p>";
+                validationResult.innerHTML = "<h2><b>Listagem de todas as TAGS das contas:</b></h2>";
                 allTags.forEach(tagName => {
                     const accountsMissingTag = [];
                     tagsByBeneficiary.forEach((tagList, beneficiaryName) => {
@@ -206,15 +194,19 @@ function compararContas() {
                         }
                     });
                     const status = accountsMissingTag.length === 0 ? "Presente em todas as contas" : `Não encontrada em ${accountsMissingTag.join(", ")}`;
-                    validationResult.innerHTML += `<p><strong>${tagName}</strong>: ${status}</p>`;
+                    const resultElement = document.createElement('p');
+                    resultElement.innerHTML = `<strong>${tagName}</strong>: ${status}`;
+                    if (accountsMissingTag.length === 0) {
+                        resultElement.classList.add('present');
+                    } else {
+                        resultElement.classList.add('absent');
+                    }
+                    validationResult.appendChild(resultElement);
                 });
 
                 // Atualizar interface de usuário
                 feedback.textContent = "";
                 clearResultBtn.classList.remove('hidden');
-                historyBtn.classList.remove('hidden');
-                historyContent.innerHTML += `<div><strong>Comparação de contas:</strong> ${new Date().toLocaleString()}</div>`;
-                historyContent.innerHTML += validationResult.innerHTML;
             } else {
                 feedback.textContent = "Erro: Arquivo XML inválido.";
             }
@@ -225,11 +217,26 @@ function compararContas() {
     }
 }
 
-function updateFeedbackMessage(message, isSuccess) {
-    const feedback = document.getElementById('feedback');
-    feedback.textContent = message;
-    feedback.classList.remove(isSuccess ? 'error' : 'success');
-    feedback.classList.add(isSuccess ? 'success' : 'error');
+function clearFile() {
+    clearFeedback();
+    document.getElementById('xmlFileInput').value = "";
+    document.getElementById('output').innerHTML = "";
+    document.getElementById('validationResult').innerHTML = "";
+    document.getElementById('feedback').innerHTML = "";
+    document.getElementById('clearResultBtn').classList.add('hidden');
+    globalBeneficiaryNames = []; // Limpa a variável global que armazena os nomes dos beneficiários
+}
+
+function clearResult() {
+    clearFeedback();
+    document.getElementById('output').innerHTML = "";
+    document.getElementById('validationResult').innerHTML = "";
+    document.getElementById('feedback').innerHTML = "";
+    document.getElementById('clearResultBtn').classList.add('hidden');
+}
+
+function clearTag() {
+    document.getElementById('tagNameInput').value = "";
 }
 
 function clearFeedback() {
@@ -238,4 +245,3 @@ function clearFeedback() {
     feedback.classList.remove('success');
     feedback.classList.remove('error');
 }
-
